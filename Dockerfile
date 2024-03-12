e8dc4f41c23a4339df6aaf847ce801d31f80a432
Shaoqian Zhou <zhouushaooqiann@outlook.com>
2024-03-11 19:56:47 -0500

    fixing GAT

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
-#FROM nvidia/cuda:12.3.2-devel-ubuntu20.04
-FROM ubuntu:20.04
+FROM nvidia/cuda:12.3.2-cudnn9-devel-ubuntu20.04
+#FROM nvidia/cuda:11.3.1-cudnn8-devel-ubuntu20.04
+#FROM ubuntu:20.04
 
 WORKDIR /
 
 RUN apt-get update && apt-get install wget -yq
 RUN apt-get cmake && apt-get install ninja-build && apt-get clang
 RUN wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/llvm-project-11.0.1.src.tar.xz
+RUN tar -xvf llvm-project-11.0.1.src.tar.xz
+RUN cd llvm-project-11.0.1.src
+#RUN mkdir llvm-project-11.0.1.src/build
+RUN cmake -S llvm -B build -G Ninja -DLLVM_ENABLE_PROJECTS="clang;lld"
+RUN ninja -C build check-llvm
--- hec_gnn/conv/GAT.py
+++ hec_gnn/conv/GAT.py
@@ -14,9 +14,10 @@ from torch_geometric.typing import PairTensor, Adj, OptTensor, Size, OptPairTens
 from torch_sparse import SparseTensor, matmul
 from torch_geometric.data import Data
 from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.utils import remove_self_loops, add_self_loops, softmax
 import __init__
 from utils.base_func import masked_edge_index,masked_edge_attr
-
+import pdb
 
 class HECGATConv(MessagePassing):
     def __init__(self, in_channels: Union[int, Tuple[int,int]], out_channels: int, dim=1, num_relations=1,
@@ -39,8 +40,12 @@ class HECGATConv(MessagePassing):
             self.relation_weight.append(nn.Linear(in_channels[0], out_channels * num_heads, bias=False))
             self.relation_att.append(nn.Linear(2 * out_channels, num_heads, bias=False))
         
+        # 
+        self.lin_
         self.lin_r = nn.Linear(in_channels[1], out_channels * num_heads, bias=False)
-        self.attr_fc = nn.Linear(dim, in_channels[0], bias=False)
+        
+        # used to map edge features into the same dimension as node features
+        self.attr_fc = nn.Linear(dim, in_channels[1], bias=False)
         
         if bias and concat:
             self.bias = nn.Parameter(torch.Tensor(num_heads * out_channels))
@@ -62,22 +67,22 @@ class HECGATConv(MessagePassing):
         if self.bias is not None:
             nn.init.zeros_(self.bias)
     
-    def forward(self, x, edge_index, edge_weight=None, edge_type=None, size=None):
-        if isinstance(x, torch.Tensor):
-            x = (x, x)
-        
+    def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
+                edge_weight: OptTensor = None, edge_type: OptTensor = None,size: Size = None):
+        if isinstance(x, Tensor):
+            x: OptPairTensor = (x, x)
         out = torch.zeros(x[0].size(0), self.num_heads, self.out_channels, device=x[0].device)
         
         if self.dim > 1:
             edge_weight = self.attr_fc(edge_weight)
         
+        pdb.set_trace()
         # iterate through each relation type, with separate relation weight and attention         
-        for i in range(self.num_relation):
-            edge_index_i = edge_index[:, edge_type == i]
-            edge_weight_i = edge_weight[edge_type == i] if edge_weight is not None else None
+        for i in range(self.num_relations):
+            tmp = masked_edge_index(edge_index, edge_type == i)
             
-            x_i = self.propagate(edge_index_i, x=x, edge_weight=edge_weight_i, relation_idx=i)
-            out += x_i.view(-1, self.num_heads, self.out_channels)
+            tmp_out = self.propagate(tmp, x=x, edge_weight=masked_edge_attr(edge_weight, edge_type == i), size=size, relation_idx=i)
+            out += tmp_out.view(-1, self.num_heads, self.out_channels)
         
         if x[1] is not None:
             out += self.lin_r(x[1]).view(-1, self.num_heads, self.out_channels)
@@ -92,18 +97,38 @@ class HECGATConv(MessagePassing):
         
         return out
     
-    def message(self, edge_index_i, x_i, x_j, edge_weight_i, relation_idx):
-        x_j = self.relation_weight[relation_idx](x_j).view(-1, self.num_heads, self.out_channels)
-        if edge_weight_i is not None:
-            edge_weight_i = edge_weight_i.view(-1, 1, 1)
-            x_j = edge_weight_i * x_j
+    def message(self, edge_index, x_i, x_j, edge_weight, relation_idx):
+        # x_j = self.relation_weight[relation_idx](x_j).view(-1, self.num_heads, self.out_channels)
+        # if edge_weight_i is not None:
+        #     edge_weight_i = edge_weight_i.view(-1, 1, 1)
+        #     x_j = edge_weight_i * x_j
         
+        
+        
+        pdb.set_trace()
+        
+        
+        # apply relational weight (linear layer) to the edge embeddings, as well as target and source node embeddings
+        edge_weight_msg = self.relation_weight[relation_idx](msg).view(-1, self.num_heads, self.out_channels)
+        x_i_msg = 
+        x_j_msg =    
+
+        # use edge attributes as message
+        if edge_weight is not None:
+            msg = edge_weight
+        else:
+            msg = x_j
+
+        
+
+        # still use source and target node features to calculate attention
         alpha = torch.cat([x_i, x_j], dim=-1)
         alpha = self.relation_att[relation_idx](alpha)
         alpha = F.leaky_relu(alpha, negative_slope=0.2)
-        alpha = softmax(alpha, edge_index_i[0])
+        alpha = softmax(alpha, edge_index[0])
         
-        return x_j * alpha.view(-1, self.num_heads, 1)
+        # output in the shape of (N, num_heads, out_channels)
+        return msg * alpha.view(-1, self.num_heads, 1) 
     
     def update(self, aggr_out):
         return aggr_out
